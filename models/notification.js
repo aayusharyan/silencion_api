8fccf1eb394f8f06ea934f60b92e915ffd55464f
Aayush Sinha <aayush.aryan@me.com>
2017-04-01 12:45:42 +0530

    Git ki maa ki chut

--- models/notification.js
+++ models/notification.js
@@ -1,8 +1,8 @@
 module.exports.handle_notif = function (req, res) {
     var lodash = require("lodash");
-    var departures = require("../departures.json");
+    var departures = require("../pnr.json");
     var pnr = req.params.pnr;
     var picked = lodash.filter(departures, { 'pnr': pnr } );
-    console.log (picked);
+    console.log (picked.flight_id);
     res.send ("STATUS CODE: 200");
 }
