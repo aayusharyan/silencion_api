38a2d116e02d0d060dcf3257017e21c4b4b51887
Aayush Sinha <aayush.aryan@me.com>
2017-04-02 12:12:56 +0530

    API Configuration Updated

--- models/departures.js
+++ models/departures.js
@@ -1,40 +1,86 @@
 module.exports.list_all = function (req, res) {
     var arrivals = require("../departures.json");
-    res.send (JSON.stringify (arrivals));
+    res.send(JSON.stringify(arrivals));
 }
 
-module.exports.search =  function (req, res) {
+module.exports.search = function (req, res) {
     var lodash = require("lodash");
     var departures = require("../departures.json");
     var pnr_list = require("../pnr.json");
-    var pnr = req.params.pnr;
-    var picked = lodash.filter(pnr_list, { 'pnr': pnr } );
-    var flight_id = picked[0].flight_id;
-    var flight_details = lodash.filter(departures, {'id': flight_id});
-    var single_flight_details_obj = flight_details[0];
-    
-    var airports = require("../locations.json");
-    var airports_loc = lodash.filter(airports, {'code': single_flight_details_obj.destination});
-    var dest_long = airports_loc[0].long;
-    var dest_lat = airports_loc[0].lat;
-    
-    
-    var result_object = {};
-    result_object.id = single_flight_details_obj.id;
-    result_object.source = single_flight_details_obj.source;
-    result_object.destination = single_flight_details_obj.destination;
-    result_object.dest_lat = dest_lat;
-    result_object.dest_long = dest_long;
-    result_object.seat = single_flight_details_obj.seat;
-    result_object.company = single_flight_details_obj.company;
-    result_object.seat = single_flight_details_obj.seat;
-    result_object.company = single_flight_details_obj.company;
-    result_object.logo = single_flight_details_obj.logo;
-    result_object.flightNo = single_flight_details_obj.flightNo;
-    result_object.pnr = pnr;
-    result_object.gate = single_flight_details_obj.gate;
-    result_object.eta = single_flight_details_obj.eta;
-    result_object.delay = single_flight_details_obj.delay;
-    result_object.comments = single_flight_details_obj.comments;
-    res.send (JSON.stringify (result_object));
+    var search_query = req.params.query;
+    var picked = lodash.filter(pnr_list, {
+        'pnr': search_query
+    });
+    if (picked.length == 1) {
+        var flight_id = picked[0].flight_id;
+        var flight_details = lodash.filter(departures, {
+            'id': flight_id
+        });
+        var single_flight_details_obj = flight_details[0];
+
+        var airports = require("../locations.json");
+        var airports_loc = lodash.filter(airports, {
+            'code': single_flight_details_obj.destination
+        });
+        var dest_long = airports_loc[0].long;
+        var dest_lat = airports_loc[0].lat;
+
+
+        var result_object = {};
+        result_object.id = single_flight_details_obj.id;
+        result_object.source = single_flight_details_obj.source;
+        result_object.destination = single_flight_details_obj.destination;
+        result_object.dest_lat = dest_lat;
+        result_object.dest_long = dest_long;
+        result_object.seat = single_flight_details_obj.seat;
+        result_object.company = single_flight_details_obj.company;
+        result_object.seat = single_flight_details_obj.seat;
+        result_object.company = single_flight_details_obj.company;
+        result_object.logo = single_flight_details_obj.logo;
+        result_object.flightNo = single_flight_details_obj.flightNo;
+        result_object.pnr = search_query;
+        result_object.gate = single_flight_details_obj.gate;
+        result_object.eta = single_flight_details_obj.eta;
+        result_object.delay = single_flight_details_obj.delay;
+        result_object.comments = single_flight_details_obj.comments;
+        res.send(JSON.stringify(result_object));
+    } else if (picked.length == 0) {
+
+
+        var flight_details = lodash.filter(departures, {
+            'flightNo': search_query.toUpperCase()
+        });
+        if (flight_details.length == 1) {
+
+
+        var single_flight_details_obj = flight_details[0];
+
+        var airports = require("../locations.json");
+        var airports_loc = lodash.filter(airports, {
+            'code': single_flight_details_obj.destination
+        });
+        var dest_long = airports_loc[0].long;
+        var dest_lat = airports_loc[0].lat;
+
+
+        var result_object = {};
+        result_object.id = single_flight_details_obj.id;
+        result_object.source = single_flight_details_obj.source;
+        result_object.destination = single_flight_details_obj.destination;
+        result_object.dest_lat = dest_lat;
+        result_object.dest_long = dest_long;
+        result_object.company = single_flight_details_obj.company;
+        result_object.logo = single_flight_details_obj.logo;
+        result_object.flightNo = single_flight_details_obj.flightNo;
+        result_object.gate = single_flight_details_obj.gate;
+        result_object.eta = single_flight_details_obj.eta;
+        result_object.delay = single_flight_details_obj.delay;
+        result_object.comments = single_flight_details_obj.comments;
+        res.send(JSON.stringify(result_object));
+        } else {
+            res.sendStatus (404);
+        }
+    }
+
+
 }
--- routes/routes.js
+++ routes/routes.js
@@ -3,7 +3,7 @@ var appRouter = function (app) {
     app.get("/", require("../models/landing_page").main);
     app.get("/arrivals/", require("../models/arrivals").list_all);
     app.get("/departures/", require("../models/departures").list_all);
-    app.get("/departures/:pnr", require("../models/departures").search);
+    app.get("/departures/:query", require("../models/departures").search);
     app.get("/offers/", require("../models/offers").list_all);
     app.get("/offers/:search_query", require("../models/offers").search);
     app.get("/notif_handler/:pnr", require("../models/notification").handle_notif);
